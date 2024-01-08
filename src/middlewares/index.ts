7f77c4a7b2ed725c5db01fe799a70971026ab742
Moriartti <92302869+MoriiSan@users.noreply.github.com>
2024-01-09 06:14:35 +0800

    fix delete

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -39,6 +39,7 @@ export const isAuthenticated = async (req: express.Request, res: express.Respons
 
         merge(req, {identity: existingUser});
 
+        next();
     } catch (error) {
         console.log(error);
         return res.sendStatus(400);
